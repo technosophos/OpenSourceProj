1e4b4a1ece1dc254f973d59546c5c9a145c39eef
Matt Butcher <technosophos@gmail.com>
2014-02-27 08:37:25 -0700

    Fixed my indenting problem.

--- src/Hola.java
+++ src/Hola.java
@@ -5,11 +5,11 @@ public class Hola {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-     if (args.length > 0 && "gringo".equals(args[0])) {
-        System.out.println("Hello World");
-     } else {
+		if (args.length > 0 && "gringo".equals(args[0])) {
+		    System.out.println("Hello World");
+		} else {
 		    System.out.println("Hola Mundo!");
-		 }
+		}
 	}
 }
 
